630810ba6c2f04571a475220d17275f2ff290f96
deathcrow999 <deathcrow4561@gmail.com>
2015-09-18 12:03:17 -0400

    Update BasicPlugin.php

--- src/LiveWireTech/BasicPlugin/BasicPlugin.php
+++ src/LiveWireTech/BasicPlugin/BasicPlugin.php
@@ -1,1 +1,16 @@
 <?php
+
+namespace LiveWireTech\BasicPlugin;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class BasicPlugin extends PluginBase{
+  public function onCommand(CommandSender $sender,Command $cmd, $label, array $args){
+    $cmdName = $cmd->getName();
+    if($cmdName === "greetme"){
+      $sender->sendMessage("Sent a greetings message")
+    }
+  }
+}
